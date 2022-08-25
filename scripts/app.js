c3ad1e48cff00af04358d08eabca632b483b8586
victor sagrista <victorsagristalopez@gmail.com>
2022-08-25 11:19:32 +0200

    make form get data from user

--- scripts/app.js
+++ scripts/app.js
@@ -9,10 +9,20 @@ class Book {
     }
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+function addBookToLibrary() {
+    let title = this.event.target.elements[0].value;
+    let author = this.event.target.elements[1].value;
+    let pages = this.event.target.elements[2].value;
+    let read = this.event.target.elements[3].checked
+
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    loadLibrary();
 }
 
 function loadLibrary() {
     // load from array
+    console.log('loading library', myLibrary)
 }
+
+
