ad2aa6a4e81573578174c7efa6fca13de0dc3714
victor sagrista <victorsagristalopez@gmail.com>
2022-08-25 12:04:44 +0200

    save books functionality set up

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,17 @@
-let myLibrary = [];
+let myLibrary = [
+    { 
+        title: 'Harry Potter and the Sorcerer\'s Stone',
+        author: 'JK Rowling',
+        pages: 309,
+        read: true
+    },
+    { 
+        title: 'The Fellowship of the Ring',
+        author: 'JRR Tolking',
+        pages: 500,
+        read: true
+    }
+];
 
 class Book {
     constructor(title, author, pages, read) {
@@ -10,6 +23,7 @@ class Book {
 }
 
 function addBookToLibrary() {
+
     let title = this.event.target.elements[0].value;
     let author = this.event.target.elements[1].value;
     let pages = this.event.target.elements[2].value;
@@ -17,12 +31,31 @@ function addBookToLibrary() {
 
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
+
+    cleanLibrary();
     loadLibrary();
 }
 
-function loadLibrary() {
-    // load from array
-    console.log('loading library', myLibrary)
+function cleanLibrary() {
+    document.querySelector('#book-list').innerHTML = '';
 }
+
+function loadLibrary() {
+    myLibrary.forEach((book) => {
+        let listedItem = document.createElement( 'html' );
+        listedItem.innerHTML = `<li class="collection-item avatar"><i class="material-icons circle">done_all</i>
+                    <span class="title">${ book.title }</span>
+                    <p>${ book.author }<br>
+                    Pages: ${ book.pages }
+                    </p>
+                    <a href="#!" class="secondary-content"><i class="material-icons">delete</i></a>
+                </li>`
+        document.querySelector('#book-list').append(listedItem);
+    })
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    loadLibrary();
+});
 
 
